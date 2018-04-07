9bdfec129353bc85d64deb7ebc7900a57179a744
lijk <lijk@rd.netease.com>
2018-04-08 00:04:52 +0800

    ADD:complete hotfix code.

--- BuildSrc/src/main/groovy/com/lijiankun24/plugin/PreDexTransform.groovy
+++ BuildSrc/src/main/groovy/com/lijiankun24/plugin/PreDexTransform.groovy
@@ -12,6 +12,10 @@ public class PreDexTransform extends Transform {
     // 添加构造，为了方便从plugin中拿到project对象，待会有用
     public PreDexTransform(Project project) {
         this.project = project
+        // 获取到hack module的debug目录，也就是Antilazy.class所在的目录
+        def libPath = project.project(':hack').buildDir.absolutePath.concat("\\intermediates\\classes\\debug")
+        Inject.appendClassPath(libPath)
+        Inject.appendClassPath("/Users/lijiankun/Library/Android/sdk/platforms/android-26/android.jar")
     }
 
     // Transfrom在Task列表中的名字
@@ -49,31 +53,39 @@ public class PreDexTransform extends Transform {
 
         // Transfrom的inputs有两种类型，一种是目录，一种是jar包，要分开遍历
 
-        inputs.each {TransformInput input ->
+        // 遍历transfrom的inputs
+        // inputs有两种类型，一种是目录，一种是jar，需要分别遍历。
+        inputs.each { TransformInput input ->
+            input.directoryInputs.each { DirectoryInput directoryInput ->
 
-            input.directoryInputs.each {DirectoryInput directoryInput->
+                //TODO 注入代码
+                Inject.injectDir(directoryInput.file.absolutePath)
 
-                //TODO 这里可以对input的文件做处理，比如代码注入！
-
-                // 获取output目录
                 def dest = outputProvider.getContentLocation(directoryInput.name,
                         directoryInput.contentTypes, directoryInput.scopes, Format.DIRECTORY)
-
                 // 将input的目录复制到output指定目录
                 FileUtils.copyDirectory(directoryInput.file, dest)
             }
 
-            input.jarInputs.each {JarInput jarInput->
+            input.jarInputs.each { JarInput jarInput ->
 
-                //TODO 这里可以对input的文件做处理，比如代码注入！
+                //TODO 注入代码
+                String jarPath = jarInput.file.absolutePath;
+                String projectName = project.rootProject.name;
+                if (jarPath.endsWith("classes.jar")
+                        && jarPath.contains("exploded-aar\\" + projectName)
+                        // hotpatch module是用来加载dex，无需注入代码
+                        && !jarPath.contains("exploded-aar\\" + projectName + "\\hotpatch")) {
+                    Inject.injectJar(jarPath)
+                }
 
                 // 重命名输出文件（同目录copyFile会冲突）
                 def jarName = jarInput.name
                 def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
-                if(jarName.endsWith(".jar")) {
-                    jarName = jarName.substring(0,jarName.length()-4)
+                if (jarName.endsWith(".jar")) {
+                    jarName = jarName.substring(0, jarName.length() - 4)
                 }
-                def dest = outputProvider.getContentLocation(jarName+md5Name, jarInput.contentTypes, jarInput.scopes, Format.JAR)
+                def dest = outputProvider.getContentLocation(jarName + md5Name, jarInput.contentTypes, jarInput.scopes, Format.JAR)
                 FileUtils.copyFile(jarInput.file, dest)
             }
         }
---
+++ hack/src/main/java/com/lijiankun24/hack/AntilazyLoad.java
@@ -0,0 +1,10 @@
+package com.lijiankun24.hack;
+
+/**
+ * AntilazyLoad.java
+ * <p>
+ * Created by lijiankun on 18/4/7.
+ */
+
+public class AntilazyLoad {
+}
